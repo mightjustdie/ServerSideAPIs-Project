517bfff28dccb6bf270aaa169676fe4d332c8058
unknown <brenthomas02@gmail.com>
2023-02-08 23:18:19 -0500

    Buttons of previous searches and functionallity works

--- Develop/script.js
+++ Develop/script.js
@@ -8,20 +8,51 @@ var forecast = $("#weatherForecast");
 var citiesArray;
 
 $(document).ready(function() {
+    // Check if citiesArray exists in local storage, if not create an empty array
+    if (localStorage.getItem("citiesArray") === null) {
+      citiesArray = [];
+    } else {
+      citiesArray = JSON.parse(localStorage.getItem("citiesArray"));
+    }
+  
+    // Display previously searched cities
+    displayCities();
+  
     $("#searchForm").submit(function(event) {
       event.preventDefault();
-    var cityName = $("#cityInput").val();
+      var cityName = $("#cityInput").val();
+      returnForecast(cityName);
+  
+      // Add the new city to the citiesArray and update local storage
+      citiesArray.push(cityName);
+      localStorage.setItem("citiesArray", JSON.stringify(citiesArray));
+  
+      // Display the updated list of cities
+      displayCities();
+    });
+  });
+  
+  // Displays clicked button's data
+  $(document).on("click", ".city-button", function() {
+    var cityName = $(this).attr("data-city");
     returnForecast(cityName);
   });
-});
-  
 
-// Call API for lat and lon by city name
+  function displayCities() {
+    $("#previousCities").empty();
+    for (var i = 0; i < citiesArray.length; i++) {
+      var cityButton = $("<button>").text(citiesArray[i]);
+      cityButton.addClass("city-button btn btn-primary m-1");
+      cityButton.attr("data-city", citiesArray[i]);
+      $("#previousCities").append(cityButton);
+    }
+  }
+// Call forecast api by city name
 function returnForecast(cityName) {
     let queryURL = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${apiKey}&units=imperial`
     
-    fetch (queryURL)
-    .then(function(response){
+    fetch (queryURL).then(function(response){
+
         return response.json();
     })
     .then(function(data){
