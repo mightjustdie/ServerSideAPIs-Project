27947e03fc81970cdacf26d5c4bf89d79c09da34
unknown <brenthomas02@gmail.com>
2023-02-08 22:09:12 -0500

    When previous search exists click function works

--- Develop/script.js
+++ Develop/script.js
@@ -7,29 +7,26 @@ var forecast = $("#weatherForecast");
 // City Array //
 var citiesArray;
 
+// Search functionallity
 $(document).ready(function() {
-    $("#cityForm").submit(function(event) {
+    $("#searchForm").submit(function(event) {
       event.preventDefault();
     var cityName = $("#cityInput").val();
     returnLatLon(cityName);
   });
 });
-  
 
-// Call API for current weather by lat and lon
-function returnCurrentWeather(lat, lon) {
-    let queryURL = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`;
-    fetch(queryURL)
-    .then(function(response){
-        return response.json();
-    })
-    .then(function(data){
-        console.log(data)
-    })
-}
+// Pulls up City data when city is clicked
+$(document).ready(function(){
+$("#previousSearch").click(function(event) {
+    let cityName = event.target.value
+    returnLatLon(cityName);
+});
+});
+
 // Call API for lat and lon by city name
 function returnLatLon(cityName) {
-    let queryURL = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${apiKey}`
+    let queryURL = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${apiKey}&units=imperial`
     fetch (queryURL)
     .then(function(response){
         return response.json();
@@ -38,5 +35,16 @@ function returnLatLon(cityName) {
         let lat = data.city.coord.lat;
         let lon = data.city.coord.lon;
         returnCurrentWeather(lat, lon);
+    })
+}
+// Call API for current weather by lat and lon
+function returnCurrentWeather(lat, lon) {
+    let queryURL = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}&units=imperial`;
+    fetch(queryURL)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(data){
+        console.log(data)
     })
 }
