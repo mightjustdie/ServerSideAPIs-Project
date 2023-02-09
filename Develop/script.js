e09a2ded5573bb8afcf0cbdf9643bef1b402ef75
unknown <brenthomas02@gmail.com>
2023-02-08 20:40:24 -0500

    I forgot to make a repo...

---
+++ Develop/script.js
@@ -0,0 +1,28 @@
+// Openweather API Key //
+const apiKey = "52c57acb0a101e01596874761d244dca";
+// Current Weather Info //
+var currWeather = $("#currentWeather");
+// Five Day Forcast Info //
+var forecast = $("#weatherForecast");
+// City Array //
+var citiesArray;
+
+
+
+// Call API for current weather by lat and lon
+function returnCurrentWeather(lat,lon) {
+    let queryURL = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+}
+// Call API for lat and lon by city name
+function returnLatLon(cityName = 'atlanta') {
+    let queryURL = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${apiKey}`
+    fetch (queryURL)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(data){
+        console.log(data)
+    })
+    returnCurrentWeather()
+}
+returnLatLon();
